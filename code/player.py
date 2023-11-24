cd5f01a4b99229475f3dfa1ac879d21020139fc8
Tristan Luron <luront@gmail.com>
2023-11-24 08:56:29 +0100

    fire spell mechanics

--- code/player.py
+++ code/player.py
@@ -198,6 +198,11 @@ class Player(Entity):
     base_damage = self.stats["attack"]
     weapon_damage = weapon_data[self.weapon]["damage"]
     return base_damage + weapon_damage
+  
+  def get_full_magic_damage(self):
+    base_damage = self.stats["magic"]
+    spell_damage = magic_data[self.magic]["strength"]
+    return base_damage + spell_damage
  
   def energy_recovery(self):
     if self.energy < self.stats["energy"]:
