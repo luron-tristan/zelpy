d035f8f6f4451aed29f320e644f6b27e6b941f46
Tristan Luron <luront@gmail.com>
2023-11-28 20:20:59 +0100

    init xp system

--- code/level.py
+++ code/level.py
@@ -10,12 +10,14 @@ from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer
 from magic import MagicPlayer
+from upgrade import Upgrade
 
 class Level:
   def __init__(self):
 
     # get the display surface
     self.display_surface = pygame.display.get_surface()
+    self.game_paused = False
 
     # sprite group setup
     self.visible_sprites = YSortCameraGroup()
@@ -31,6 +33,7 @@ class Level:
 
     # user interface
     self.ui = UI()
+    self.upgrade = Upgrade(self.player)
 
     # particles
     self.animation_player = AnimationPlayer()
@@ -132,15 +135,24 @@ class Level:
     self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
 
   def run(self):
-    # update and draw the game
     self.visible_sprites.custom_draw(self.player)
-    self.visible_sprites.update()
-    self.visible_sprites.enemy_update(self.player)
-    self.player_attack_logic()
     self.ui.display(self.player)
+
+    if self.game_paused:
+      # display upgrade menu
+      self.upgrade.display()
+    else:
+      # run the game
+      self.visible_sprites.update()
+      self.visible_sprites.enemy_update(self.player)
+      self.player_attack_logic()
+
 
   def add_exp(self, amount):
     self.player.exp += amount
+  
+  def toggle_menu(self):
+    self.game_paused = not self.game_paused
 
 class YSortCameraGroup(pygame.sprite.Group):
   def __init__(self):
--- code/main.py
+++ code/main.py
@@ -20,6 +20,10 @@ class Game:
         if event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_r:
+            self.level.toggle_menu()
 
       self.screen.fill("black")
       self.level.run()
