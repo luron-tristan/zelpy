315151cf275d104bc6605ed83696b6d1bcc0132b
Tristan Luron <luront@gmail.com>
2023-11-30 20:47:36 +0100

    upgrade system wip

--- code/upgrade.py
+++ code/upgrade.py
@@ -8,7 +8,13 @@ class Upgrade:
     self.player = player
     self.attribute_number = len(player.stats)
     self.attribute_names = list(player.stats.keys())
+    self.max_values = list(player.max_stats.values())
     self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+    # item creation
+    self.height = self.display_surface.get_size()[1] * 0.8
+    self.width = self.display_surface.get_size()[0] // 6
+    self.create_items()
 
     # selection system
     self.selection_index = 0
@@ -39,6 +45,51 @@ class Upgrade:
        if current_time - self.selection_time >= 300:
          self.can_move = True
   
+  def create_items(self):
+    self.item_list = []
+
+    for item, index in enumerate(range(self.attribute_number)):
+      # horizontal position
+      full_width = self.display_surface.get_size()[0]
+      increment = full_width // self.attribute_number
+      left = (item * increment) + (increment - self.width) // 2
+
+      # vertical position
+      top = self.display_surface.get_size()[1] * 0.1
+
+      # create the object
+      item = Item(left, top, self.width, self.height, index, self.font)
+      self.item_list.append(item)
+
   def display(self):
     self.input()
     self.selection_cooldown()
+
+    for index, item in enumerate(self.item_list):
+      # get attributes
+      name = self.attribute_names[index]
+      value = self.player.get_value_by_index(index)
+      max_value = self.max_values[index]
+      cost = self.player.get_cost_by_index(index)
+
+      item.display(self.display_surface, self.selection_index, name, value, max_value, cost)
+
+class Item:
+  def __init__(self, l, t, w, h, i, font):
+    self.rect = pygame.Rect(l, t, w, h)
+    self.index = i
+    self.font = font
+  
+  def display_names(self, surface, name, cost, selected):
+    # title
+    title_surf = self.font.render(name, False, TEXT_COLOR)
+    title_rect = title_surf.get_rect(midtop = self.rect.midtop + pygame.math.Vector2(0, 20))
+
+    # cost
+
+    # draw
+    surface.blit(title_surf, title_rect)
+  
+  def display(self, surface, selection_num, name, value, max_value, cost):
+    pygame.draw.rect(surface, UI_BG_COLOR, self.rect)
+    self.display_names(surface, name, cost, False)
